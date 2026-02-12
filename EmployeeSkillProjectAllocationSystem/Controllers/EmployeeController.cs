5e3d33169c9a3ea9731a22f2d30b34bf3a08a80d
Universe <Universe@DESKTOP-6D5R4T4>
2026-02-12 23:02:02 +0400

    Employee changes1

---
+++ EmployeeSkillProjectAllocationSystem/Controllers/EmployeeController.cs
@@ -0,0 +1,59 @@
+﻿using EmployeeSkillProjectAllocationSystem.Data;
+using EmployeeSkillProjectAllocationSystem.Models;
+using EmployeeSkillProjectAllocationSystem.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeSkillProjectAllocationSystem.Controllers
+{
+    [Authorize(Roles = "HR")]
+    public class EmployeeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public EmployeeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            var EmpVM = new EmployeeCreateViewModel();
+
+            EmpVM.Skills = await _context.Skills
+                .Where(x => x.IsActive)
+                .Select(x => new SelectListItem {
+                    Value = x.Id.ToString(),
+                    Text = x.Name
+                })
+                .ToListAsync();
+            return View(EmpVM);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(EmployeeCreateViewModel EmpVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(EmpVM);
+            }
+            var employee = new Employee
+            {
+                Name = EmpVM.Name
+            };
+            _context.Employees.Add(employee);
+            await _context.SaveChangesAsync();
+
+            foreach(var skillId in EmpVM.SelectedSkillId)
+            {
+                _context.EmployeeSkills.Add(new EmployeeSkill
+                {
+                    EmployeeId = employee.Id,
+                    skillId = skillId
+                });
+            }
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ EmployeeSkillProjectAllocationSystem/ViewModels/EmployeeCreateViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace EmployeeSkillProjectAllocationSystem.ViewModels
+{
+    public class EmployeeCreateViewModel
+    {
+        public string Name { get; set; }
+
+        public List<int> SelectedSkillId {  get; set; } = new List<int>();
+        public List<SelectListItem> Skills {  get; set; } = new List<SelectListItem> { };
+    }
+}
